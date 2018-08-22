143e4836ebdf09a3ad502f91799ace6f69675743
curry <dsbcnw@gmail.com>
2018-08-22 23:40:17 +0800

    添加tablayout和viewpager

--- app/src/main/java/com/example/a/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a/myapplication/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.a.myapplication;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        viewPager = findViewById(R.id.vp);
+        tabLayout = findViewById(R.id.tl);
+
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 }
